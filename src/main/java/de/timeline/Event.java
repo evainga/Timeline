e049c346900131f6490f7c2583121ed3b59aa268
evainga <ego87@web.de>
2017-01-04 21:15:49 +0100

    add Timeline to github

---
+++ src/main/java/de/timeline/Event.java
@@ -0,0 +1,20 @@
+package de.timeline;
+
+import java.time.LocalDateTime;
+
+public class Event {
+
+	String eventName;
+	LocalDateTime eventDate;
+
+	public void setEvent(final String name, final LocalDateTime date) {
+		this.eventName = name;
+		this.eventDate = date;
+	}
+
+	public static void main(String[] args) {
+		Event eventExample = new Event();
+		eventExample.setEvent("Weihnachten", LocalDateTime.of(2016, 12, 24, 18, 0, 0));
+		java.lang.System.out.println(eventExample.eventDate + ": " + eventExample.eventName);
+	}
+}
