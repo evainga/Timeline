07691589a044728394287b70e5a22fede212ce06
Eva <evainga>
2017-01-12 16:04:04 +0100

    Resolve Maven verify issue

--- src/main/java/de/timeline/Event.java
+++ src/main/java/de/timeline/Event.java
@@ -22,13 +22,4 @@ public class Event {
 	public LocalDateTime getDate() {
 		return this.eventDate;
 	}
-
-	public static void main(String[] args) {
-		Event eventExample = new Event();
-		eventExample.setName("Weihnachten");
-		eventExample.setDate(LocalDateTime.of(2016, 12, 24, 18, 0, 0));
-		java.lang.System.out.println(eventExample.eventDate + ": " + eventExample.eventName);
-		java.lang.System.out.println(eventExample.getDate() + ": " + eventExample.getName());
-
-	}
 }
