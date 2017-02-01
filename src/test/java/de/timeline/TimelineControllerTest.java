29ff85329a254c7853e9c6bdbc3e043fb6b93290
mle <marcol@web.de>
2017-02-01 22:40:24 +0100

    Introduction to mocks in tests and solution of last homework

--- src/test/java/de/timeline/TimelineControllerTest.java
+++ src/test/java/de/timeline/TimelineControllerTest.java
@@ -3,18 +3,31 @@ package de.timeline;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.not;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.testng.annotations.Test;
 
-public class TimelineControllerTest {
+import com.google.common.collect.Lists;
 
-	private TimelineController timelineController = new TimelineController();
+public class TimelineControllerTest extends MockitoTest {
+	@InjectMocks
+	private TimelineController timelineController;
+	@Mock
+	private TimelineService timelineService;
 
 	@Test
 	public void showAllEvents() {
+		// given
+		when(timelineService.getAllEvents()).thenReturn(Lists.newArrayList(new Event()));
+
+		// when
 		List<Event> allEvents = timelineController.showAllEvents();
+
+		// then
 		assertThat(allEvents, not(empty()));
 	}
 
