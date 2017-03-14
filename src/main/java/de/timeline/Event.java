f4044a288a8d77d788ce695fea6cd7c00f1384d2
mle <marcol@web.de>
2017-03-14 23:07:47 +0100

    Upgrade to Spring Boot 1.5.2

--- src/main/java/de/timeline/Event.java
+++ src/main/java/de/timeline/Event.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,8 +15,8 @@ import lombok.RequiredArgsConstructor;
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
+@JsonIgnoreProperties(value = "eventId", allowGetters = true)
 public class Event {
-	@JsonIgnore
 	private UUID eventId;
 	@Size(min = 3, message = "You must use at least 3 characters")
 	private final String eventName;
